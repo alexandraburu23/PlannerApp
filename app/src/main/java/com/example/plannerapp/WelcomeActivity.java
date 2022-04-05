69ab7d7a817fe0b15079e89c492c640cfd12c05b
alexandraburu23 <alexandraburuiana23@yahoo.ro>
2022-04-05 22:40:32 +0300

    landscape ui

--- app/src/main/java/com/example/plannerapp/WelcomeActivity.java
+++ app/src/main/java/com/example/plannerapp/WelcomeActivity.java
@@ -1,6 +1,13 @@
 package com.example.plannerapp;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+    }
 }
