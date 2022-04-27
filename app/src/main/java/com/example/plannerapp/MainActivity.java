27a35bbc2fa66caec68b77c0d3e7859bc7b2985b
Andreea Gavrila <andreeagavrila02@gmail.com>
2022-04-27 22:20:05 +0300

    facebook login

--- app/src/main/java/com/example/plannerapp/MainActivity.java
+++ app/src/main/java/com/example/plannerapp/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.plannerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.facebook.FacebookSdk;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
